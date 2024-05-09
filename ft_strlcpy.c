177ca45cfe9210d984d13ac0db1c855a1045a690
Hugo <hugoabouda@gmail.com>
2024-05-09 17:39:30 +0200

    Corrected strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,19 +6,23 @@
 /*   By: Habouda <Habouda@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 20:09:12 by Habouda           #+#    #+#             */
-/*   Updated: 2024/05/08 20:48:09 by Habouda          ###   ########.fr       */
+/*   Updated: 2024/05/09 16:18:15 by Habouda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t	i;
 	size_t	src_len;
 
+	i = 0;
 	src_len = ft_strlen(src);
-	while (src[i] && i < size - 1)
+	if (!src || !dst)
+		return (0);
+	while (src[i] != '\0' && i < size - 1)
 	{
 		dst[i] = src[i];
 		i++;
