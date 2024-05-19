8a11fe38741bce2ee8078ba58cd9b5ea3a1c3286
Hugo <hugoabouda@gmail.com>
2024-05-20 00:27:58 +0200

    corrected memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: habouda <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/11 00:33:49 by habouda           #+#    #+#             */
-/*   Updated: 2024/05/11 01:29:03 by habouda          ###   ########.fr       */
+/*   Updated: 2024/05/20 00:23:58 by habouda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,19 @@ void	*ft_memchr(const void *str, int c, size_t n)
 	unsigned char	*ptr_str;
 	unsigned char	ptr_c;
 
-	ptr_str = (unsigned char *)str;
-	ptr_c = (unsigned char)c;
+	i = 0;
+	ptr_str = ((unsigned char *)str);
+	ptr_c = ((unsigned char)c);
 	while (i < n)
 	{
-		if (*ptr_str == ptr_c)
-			return (ptr_str);
-		ptr_str++;
+		if (*(ptr_str + i) == ptr_c)
+			return ((void *)(ptr_str + i));
 		i++;
 	}
 	return (NULL);
 }
+
+
 
 /*Used *ptr_str and ptr_c to cast
  *str and c to unsigned char and use them since memory blocks
