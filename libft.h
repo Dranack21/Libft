bbebcd9bfd1a8e750db18b10cf1f11a9b38742b1
Hugo <hugoabouda@gmail.com>
2024-05-19 03:47:35 +0200

    added header file and Makefile

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: habouda <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/27 20:55:55 by Habouda           #+#    #+#             */
-/*   Updated: 2024/05/17 16:27:43 by habouda          ###   ########.fr       */
+/*   Updated: 2024/05/19 03:44:25 by habouda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,15 +23,25 @@ int		ft_isascii(int c);
 int		ft_isdigit(char c);
 int		ft_isprint(char c);
 int		ft_toupper(int c);
+int		ft_tolower(int c);
+int		ft_strncmp(const char *str1, const char *str2, size_t length);
+int		ft_atoi(const char *str);
+int	ft_memcmp(const void *pointer1, const void *pointer2, size_t size);
 
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*calloc(size_t n, size_t size);
+void	*ft_memchr(const void *str, int c, size_t n);
+void	*ft_memset(void *s, int c, size_t n);
 
 size_t	ft_strlen(const char *str);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
 
 char	*ft_strdup(const char *source);
+char	*ft_strnstr(const char *str, const char *find, size_t slen);
+char	*ft_strchr(const char *str, int c);
+
 #endif
