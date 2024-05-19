85f8fb28770474772699875f127320e3ff78ea6c
Hugo <hugoabouda@gmail.com>
2024-05-19 03:46:44 +0200

    wip strtrim and final strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: habouda <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 03:07:48 by habouda           #+#    #+#             */
-/*   Updated: 2024/05/11 00:01:11 by habouda          ###   ########.fr       */
+/*   Updated: 2024/05/19 03:28:57 by habouda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,17 @@
 
 char	*ft_strchr(const char *str, int c)
 {
+	if (c == '\0')
+	{
+		while(*str)
+			str++;
+		return((char *)str);
+	}
 	while (*str)
 	{
 		if (*str == c)
 			return ((char *)str);
 		str++;
 	}
-	return (NULL);
+	return (0);
 }
