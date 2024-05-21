0586bc0bd2f06d8a998aa115e09d75c53d1537f9
Hugo <hugoabouda@gmail.com>
2024-05-21 21:06:09 +0200

    added strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,12 +6,50 @@
 /*   By: habouda <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 13:36:25 by habouda           #+#    #+#             */
-/*   Updated: 2024/05/21 16:54:30 by habouda          ###   ########.fr       */
+/*   Updated: 2024/05/21 21:04:12 by habouda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+char *ft_strncpy(char *dest, const char *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n && src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
 
 
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char const	*start;
+	char const	*end;
+	char		*buffer;
+	size_t		length;
 
+	if (!s1 || !set)
+		return (NULL);
+	start = s1;
+	end = s1 + ft_strlen(s1) - 1;
+	while (*start && ft_strchr(set, *start))
+		start++;
+	while (end > start && ft_strchr(set, *end))
+		end--;
+	length = end - start + 1;
+	buffer = ft_calloc(sizeof(char)* (length +1), 1);
+	if (!buffer)
+		return (NULL);
+	ft_strncpy(buffer, start , length);
+	return (buffer);
+}
