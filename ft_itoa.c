d4cea561c824d77da0f32b3d0a70afb9342b1270
Hugo <Hugoabouda@gmail.com>
2024-05-21 16:38:05 +0200

    added itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,56 +6,57 @@
 /*   By: habouda <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 12:10:23 by habouda           #+#    #+#             */
-/*   Updated: 2024/05/21 13:26:04 by habouda          ###   ########.fr       */
+/*   Updated: 2024/05/21 16:36:43 by habouda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	n_size(int	n)
+
+int	n_size(int n)
 {
 	int	i;
 
 	i = 0;
+	if (n == 0)
+		return (1);
 	if (n < 0)
 	{
 		n = -n;
 		i++;
 	}
-	while (n > 0)
+	while (n != 0)
 	{
-		n = n /10;
+		n = n / 10;
 		i++;
 	}
 	return (i);
 }
-char *ft_itoa(int n)
+
+char	*ft_itoa(int n)
 {
 	char	*s;
+	long	nb;
 	int		k;
-	int 	end;
 
-	if (n == 0)
-		return ("0");
-	if (n == -2147483648)
-		return ("-2147483648");
 	k = n_size(n);
+	nb = n;
 	s = malloc (sizeof(char) * (k + 1));
 	if (!s)
 		return (NULL);
-	end = k;
-	k = k - 1;
-	if (n < 0)
-		n = -n;
-	while (n > 0)
+	if (nb < 0)
 	{
-		s[k] = ((n % 10) + '0');
-		n = n / 10;
-		k--;
+		s[0] = '-';
+		nb = -nb;
 	}
-	s[k] = '-';
-	s[end] = '\0';
+	s[k] = '\0';
+	while (nb >= 0)
+	{
+		s[--k] = (nb % 10) + ('0');
+		nb = nb / 10;
+		if (nb == 0)
+			break;
+	}
 	return (s);
 }
-
 
