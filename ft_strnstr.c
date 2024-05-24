92fd62936d1206cb43ada5cedd0460bfbdef54b4
Hugo <Hugoabouda@gmail.com>
2024-05-24 12:20:54 +0200

    corrected stnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,37 +6,38 @@
 /*   By: habouda <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 11:36:49 by habouda           #+#    #+#             */
-/*   Updated: 2024/05/23 12:34:40 by habouda          ###   ########.fr       */
+/*   Updated: 2024/05/24 12:01:50 by habouda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *str, const char *find, size_t slen)
+char	*ft_strnstr(const char *big, const char *little, size_t slen)
 {
-	size_t		i;
-	size_t		j;
-	char		*temp;
-	char		*str2;
+	size_t	i;
+	size_t	j;
+	size_t	k;
 
 	i = 0;
 	j = 0;
-	temp = (char *)find;
-	str2 = (char *)str;
-	if (temp[j] == '\0')
-		return (str2);
-	while (str2[i] && i < slen)
+	k = 0;
+	if (little[j] == '\0')
+		return ((char *)big);
+	while (big[i] && i < slen)
 	{
-		while (temp[j] == str2[i] && temp[j] && str2[i] && i <= slen)
+		while (little[j] == big[i] && little[j] && big[i] && i < slen)
 		{
 			j++;
 			i++;
 		}
-		if (temp[j] == '\0')
-			return (&str2[i - ft_strlen(find)]);
+		if (little[j] == '\0')
+			return ((char *)&big[i - ft_strlen(little)]);
 		j = 0;
-		if (temp[j] != str2[i])
+		k++;
+		i = k;
+		if (little[j] != big[i])
 			i++;
 	}
 	return (NULL);
 }
+
