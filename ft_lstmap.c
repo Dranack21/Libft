1eb4e0064e348cb9a1c10d34ad4251e0f442fe6a
Hugo <Hugoabouda@gmail.com>
2024-05-27 12:39:12 +0200

    monkey

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,23 +6,31 @@
 /*   By: habouda <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/26 00:27:49 by habouda           #+#    #+#             */
-/*   Updated: 2024/05/26 00:47:01 by habouda          ###   ########.fr       */
+/*   Updated: 2024/05/27 12:38:42 by habouda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *),void (*del)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*ptr;
-	t_list	*temp;
+	t_list 	*newnode;
 
-	ptr = malloc(sizeof(t_list)* ft_lstsize(lst));
-	if (!ptr)
+	if (!lst || !(*f) || !(*del))
 		return (NULL);
+	ptr = NULL;
 	while (lst)
 	{
-		
+		newnode = ft_lstnew((*f)(lst->content));
+			if (!newnode)
+			{
+				ft_lstclear(&ptr, del);
+				return (NULL);
+			}
+		ft_lstadd_back (&ptr, newnode);
+		lst = lst->next;
 	}
 	return (ptr);
 }
+
